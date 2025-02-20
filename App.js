4ace8067d1897737b49ac32935da6a8f69589a1e
RivalMaulana6 <rivalmaulana990@gmail.com>
2025-02-20 23:25:29 +0700

    Fix case sensitivity issues

--- App.js
+++ App.js
@@ -1,6 +1,22 @@
 import React from "react";
 import { NavigationContainer } from "@react-navigation/native";
-import AppNavigator from "./navigation/AppNavigator";
+import AppNavigator from  "navigation/AppNavigator"; 
+import ProductList from "./components/ProductList"; // Import daftar produk
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <AppNavigator />
+      <div>
+        <h1>Light Stick Shop</h1>
+        <ProductList /> {/* Menampilkan daftar produk */}
+      </div>
+    </NavigationContainer>
+  );
+}
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import AppNavigator from  "navigation/AppNavigator"; 
 import ProductList from "./components/ProductList"; // Import daftar produk
 
 export default function App() {
